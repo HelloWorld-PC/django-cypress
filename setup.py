78a2394ab526d0ee613b99fb71ca0755dae5bdec
Stavros Panakakis <spanakakis@helloworldpc.com>
2023-06-15 17:14:05 +0300

    Add the library boilerplate
    Signed-off-by: Stavros Panakakis <spanakakis@helloworldpc.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    README = f.read()
+
+
+setup(
+    name="django-cypress",
+    version="0.0.1",
+    description="Integration of Cypress in a Django project.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="HelloWorld PC",
+    url="https://github.com/HelloWorld-PC/django-cypress",
+    license="MIT License",
+    include_package_data=True,
+    packages=find_packages(),
+    requires=[
+        "Django (>=3.2)",
+    ],
+    install_requires=[
+        "Django>=3.2",
+    ],
+)
