dd07a927551b0e15dc6d94c5313fdff3a50d2fa3
Stavros Panakakis <spanakakis@helloworldpc.com>
2023-10-29 15:42:07 +0200

    Add cy.createUser() command
    Signed-off-by: Stavros Panakakis <spanakakis@helloworldpc.com>

--- django_cypress/views.py
+++ django_cypress/views.py
@@ -1,9 +1,38 @@
 import json
+from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.core import management
 from django.http import HttpRequest, JsonResponse
 from django.middleware.csrf import get_token
 from django.views import View
+
+
+class CreateUserView(View):
+    """A view for creating a user via HTTP POST requests."""
+
+    def post(
+        self,
+        request: HttpRequest,
+    ) -> JsonResponse:
+        """Handle HTTP POST requests to create a user.
+
+        Args:
+        ----
+        request (HttpRequest): The HTTP request object containing the user data.
+
+        Returns:
+        -------
+        JsonResponse: A JSON response indicating the success of the user creation.
+        """
+        body = json.loads(request.body.decode("utf-8"))
+
+        try:
+            user_model = get_user_model()
+            user = user_model.objects.create_user(**body)
+            return JsonResponse({"user_id": user.id}, status=HTTPStatus.CREATED)
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=HTTPStatus.BAD_REQUEST)
 
 
 class RefreshDatabaseView(View):
