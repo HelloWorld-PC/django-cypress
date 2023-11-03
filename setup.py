9966e4a3e281b53a2c8b81d13c46cd2d34296fea
Stavros Panakakis <spanakakis@helloworldpc.com>
2023-11-03 16:00:54 +0200

    Bump to v1.0.1
    Signed-off-by: Stavros Panakakis <spanakakis@helloworldpc.com>

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
 
 setup(
     name="django-cypress",
-    version="1.0.0",
+    version="1.0.1",
     description="Integration of Cypress in a Django project.",
     long_description=README,
     long_description_content_type="text/markdown",
